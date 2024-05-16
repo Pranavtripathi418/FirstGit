056160320c3b6d5d096c95679651c9c8ab3595e4
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-05-17 00:40:45 +0530

    getElementsByClassName

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const fruit= document.getElementsByClassName('fruit');
 fruit[2].style.backgroundColor='yellow';
 
-const header= document.getElementById('header');
-header.style.backgroundColor="green";
-header.style.borderBottom="3px solid orange";
-
-const basketHeading= document.getElementById('basket-heading');
-basketHeading.style.color="green";
-
-const thanks= document.getElementById('thanks');
-thanks.innerHTML='<p>Please visit us again</p>';
+for(let i=0; i<=fruit.length; i++){
+    fruit[i].style.fontWeight='bold';
+}
